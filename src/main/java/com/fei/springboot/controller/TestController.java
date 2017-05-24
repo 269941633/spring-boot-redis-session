f6cad731e3c4934525854dbe50fddb03cfb9a695
Jianfei Wei <269941633@qq.com>
2017-05-24 15:48:44 +0800

    spring boot+session+redis缓存解决session共享

---
+++ src/main/java/com/fei/springboot/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.fei.springboot.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class TestController {
+
+	@RequestMapping(value="/getSessionId")
+	@ResponseBody
+	public String getSessionId(HttpServletRequest request){
+		
+		Object o = request.getSession().getAttribute("springboot");
+		if(o == null){
+			o = "spring boot 牛逼了!!!有端口"+request.getLocalPort()+"生成";
+			request.getSession().setAttribute("springboot", o);
+		}
+		
+		return "端口=" + request.getLocalPort() +  " sessionId=" + request.getSession().getId() +"<br/>"+o;
+	}
+	
+}
